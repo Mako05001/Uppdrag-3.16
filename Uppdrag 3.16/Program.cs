7e5220d847d09c4a6a2b93b4ac7e702207561e49
MAKSY <MAKSY@DESKTOP-NT4SAN5>
2022-09-24 22:59:51 +0200

    Add project files.

---
+++ Uppdrag 3.16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppdrag3._16
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur lång är din låt i minuter?");
+            string L = Console.ReadLine();
+            int m = Convert.ToInt32(L);
+            Console.WriteLine("Hur lång är din låt i sekunder?");
+            string k = Console.ReadLine();
+            int S = Convert.ToInt32(k);
+            int M = m * 60;
+            if (M + S <= 260 && M + S >= 165)
+            {
+                Console.WriteLine("Din låt får spelas på radiostationen!");
+            }
+            else
+            {
+                Console.WriteLine("Din låt kommer inte att spelas på radiostationen");
+            }
+        }
+    }
+}
